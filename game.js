c24becf646c5676eaf162d716d053117f28e0044
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-24 20:25:00 +0700

    Add UX

--- game.js
+++ game.js
@@ -2,8 +2,17 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var level = 0;
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+$(document).one("keydown", function (e) {
+    var keyPressed = e.key;
+    if (keyPressed = "a") {
+        nextSequence();
+    }
+
+});
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
@@ -11,6 +20,8 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+    level++;
+    $("h1").text("Level " + level);
 
 }
 
@@ -18,11 +29,16 @@ function nextSequence() {
 $(".btn").click(function (e) {
     var userChosenColour = (e.currentTarget.id);
     userClickedPattern.push(userChosenColour);
-    $("#" + userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play;
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () { $("#" + currentColour).removeClass("pressed"); }, 100);
 }
