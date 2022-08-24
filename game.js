e741610c0fdc49a78c5c2cff567d8b8a8d38e9af
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-24 19:40:32 +0700

    Add Sound and Flash Effect

--- game.js
+++ game.js
@@ -2,11 +2,15 @@ var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[randomNumber];
+
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
-}
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
-gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play;
+}
